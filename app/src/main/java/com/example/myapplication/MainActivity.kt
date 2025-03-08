218ebb469895773f5f1b9bc856f1d406448451de
it23319738 <mufeezmujassir80@gmail.com>
2025-03-08 16:36:31 +0530

    3rd commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.View
 import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
@@ -16,14 +18,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Find the TextView
+        //find the view
         val textView: TextView = findViewById(R.id.textView)
+        val imageView:ImageView=findViewById(R.id.imageView)
+        val largeimage:ImageView=findViewById(R.id.imageLargeView)
+        val button:Button=findViewById(R.id.button)
+        // Initially, set the small image and text to be invisible
+        imageView.visibility = View.INVISIBLE
+        textView.visibility = View.INVISIBLE
 
-        // Use a Handler to delay the visibility of the TextView
+        val fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        val fadeOut = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+
+        largeimage.startAnimation(fadeIn)
+
         Handler().postDelayed({
-            val fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+
+            largeimage.startAnimation(fadeOut)
+            largeimage.visibility=View.INVISIBLE
+
+
+
             textView.startAnimation(fadeIn)
+            imageView.startAnimation(fadeIn)
+            button.startAnimation(fadeIn)
+            button.visibility=View.VISIBLE
+            imageView.visibility=View.VISIBLE
             textView.visibility = View.VISIBLE
-        }, 20000) // 2000 milliseconds = 2 seconds
+        }, 1000) // 2000 milliseconds = 2 seconds
     }
 }

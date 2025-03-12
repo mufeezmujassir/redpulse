f4159b35ee81208fdcfff7d99f53ceff00b722eb
it23319738 <mufeezmujassir80@gmail.com>
2025-03-12 12:51:51 +0530

    5day 2ndcommit

--- app/src/main/java/com/example/myapplication/loginpage.kt
+++ app/src/main/java/com/example/myapplication/loginpage.kt
@@ -1,11 +1,25 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class loginpage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loginpage)
+        val login:Button=findViewById(R.id.login);
+        val signup:TextView=findViewById(R.id.signuptext)
+        login.setOnClickListener{
+            val intent= Intent(this@loginpage,homepage::class.java)
+            startActivity(intent)
+        }
+        signup.setOnClickListener{
+            val intent=Intent(this@loginpage,signupPage::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/myapplication/signupPage.kt
+++ app/src/main/java/com/example/myapplication/signupPage.kt
@@ -18,15 +18,11 @@ class signupPage : AppCompatActivity() {
        val signup:Button=findViewById(R.id.signup)
         val dobEditText: EditText = findViewById(R.id.dob)
         val gender: Spinner = findViewById(R.id.gender)
-        val spinner: Spinner = findViewById(R.id.spinner)
+        val spinner: Spinner = findViewById(R.id.bloodtype)
         val logintext:TextView=findViewById(R.id.logintext)
-        val adapter = ArrayAdapter.createFromResource(
-            this, R.array.spinner_items, android.R.layout.simple_spinner_item
-        )
 
-        val genderAdapter = ArrayAdapter.createFromResource(
-            this, R.array.spinner_gender, android.R.layout.simple_spinner_item
-        )
+
+
 
         val calendar = Calendar.getInstance()
         val today = calendar.timeInMillis
@@ -54,48 +50,19 @@ class signupPage : AppCompatActivity() {
             datePickerDialog.show()
         }
 
-        // Set adapters for spinners
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = adapter
-        gender.adapter = genderAdapter
 
-        gender.setSelection(0)
-        spinner.setSelection(0)
 
-        gender.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                if (position == 0) {
-                    Toast.makeText(this@signupPage, "Gender type", Toast.LENGTH_SHORT).show()
-                } else {
-                    // Handle other positions if necessary
-                }
-            }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                // Do nothing
-            }
-        }
 
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                if (position == 0) {
-                    Toast.makeText(this@signupPage, "Blood type", Toast.LENGTH_SHORT).show()
-                } else {
-                    // Handle other positions if necessary
-                }
-            }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                // Do nothing
-            }
-        }
+
         signup.setOnClickListener{
             val intent=Intent(this@signupPage,loginpage::class.java)
             startActivity(intent)
         }
         logintext.setOnClickListener{
             val intent=Intent(this@signupPage,loginpage::class.java)
+            startActivity(intent)
         }
 
 
